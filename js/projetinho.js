60c8a8043e2d31c6f124605ff80d8a81469d280c
madusantos27 <mariaeduarda93d@gmail.com>
2025-08-13 21:55:40 -0300

    alteração

--- js/projetinho.js
+++ js/projetinho.js
@@ -1,9 +1,18 @@
 function toggleMode() {
 
-    const html = document.do
+    const html = document.documentElement;
+    html.classList.toggle("ligth");
 
 
+    const img = document.querySelector("#profile img");
 
+    if(html.classList.contains("ligth")){
 
+        img.setAttribute("src", ".img/foto1.webp");
+    } else {
+
+        img.setAttribute("src", "./img/foto2.jpg");
+
+    }
 
 }
